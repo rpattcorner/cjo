f54dfe49a9f71d3023e75d5ab93ffb9174248942
rpattcorner <robert.patt-corner@eglobaltech.com>
2014-11-16 23:13:32 -0500

    add site

--- site_cookbooks/cjo_site/recipes/default.rb
+++ site_cookbooks/cjo_site/recipes/default.rb
@@ -1,7 +1,11 @@
 include_recipe "apache2"
 
+cookbook_file "cabinjohnorganizing.com" do
+	path: "/etc/apache2/sites-available/cabinjohnorganizing.com"
+	action: create
+end
+
+
 apache_site "cabinjohnorganizing.com" do
   enable true
-end
-  EOH
 end
