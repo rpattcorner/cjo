8fca38a81513d02648605ec62c53fe4c494e800e
rpattcorner <robert.patt-corner@eglobaltech.com>
2014-11-16 23:27:40 -0500

    fix path

--- site_cookbooks/cjo_site/recipes/default.rb
+++ site_cookbooks/cjo_site/recipes/default.rb
@@ -1,8 +1,8 @@
 include_recipe "apache2"
 
-cookbook_file "cabinjohnorganizing.com" do
-	path: "/etc/apache2/sites-available/cabinjohnorganizing.com"
-	action: create
+cookbook_file "/etc/apache2/sites-available/cabinjohnorganizing.co" do
+        source "/sites-available/cabinjohnorganizing.com"
+        action :create
 end
 
 
