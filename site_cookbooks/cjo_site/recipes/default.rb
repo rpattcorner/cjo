040fef4d3dc06df5d0fd712c32cfa791a5f36b64
rpattcorner <robert.patt-corner@eglobaltech.com>
2014-12-04 13:28:25 -0500

    adjust for apache cookbook v2

--- site_cookbooks/cjo_site/recipes/default.rb
+++ site_cookbooks/cjo_site/recipes/default.rb
@@ -1,7 +1,17 @@
 include_recipe "apache2"
 
-cookbook_file "/etc/apache2/sites-available/cabinjohnorganizing.com" do
-        source "/sites-available/cabinjohnorganizing.com"
+cookbook_file "/etc/apache2/sites-available/cabinjohnorganizing.com.conf" do
+        source "/sites-available/cabinjohnorganizing.com.conf"
+        action :create
+end
+
+cookbook_file "/etc/apache2/sites-available/andredsweald.org.conf" do
+        source "/sites-available/andredsweald.org.conf"
+        action :create
+end
+
+cookbook_file "/etc/apache2/sites-available/dorsetwest.com.conf" do
+        source "/sites-available/dorsetwest.com.conf"
         action :create
 end
 
@@ -9,6 +19,22 @@ remote_directory "/opt/content/cabinjohnorganizing" do
         source "html"
 end
 
+bash "copy dorsetwest image" do
+	user "root"
+	code <--EOH
+		aws s3 cp s3://cjo-content/dorsetwest.tgz /opt/dorsetwest.tgz
+		cd /opt/content
+		tar xzf ../dorsetwest.tgz
+		ln -s drupal_20141130a drupal
+	EOH
+	not_if do ::File.exists?("/opt/content/dorsetwest") end
+
 apache_site "cabinjohnorganizing.com" do
+  enable true
+end
+apache_site "andredsweald.org" do
+  enable true
+end
+apache_site "dorsetwest.com" do
   enable true
 end
