ed19f28e3cc8dac60d5fe150478e61e8f3bd287a
rpattcorner <robert.patt-corner@eglobaltech.com>
2014-12-06 09:20:46 -0500

    More apache fixes for mods and php

--- site_cookbooks/cjo_site/recipes/default.rb
+++ site_cookbooks/cjo_site/recipes/default.rb
@@ -1,4 +1,11 @@
 include_recipe "apache2"
+include_recipe "php"
+include_recipe "apache2::mod_php5"
+include_recipe "apache2::mod_proxy"
+include_recipe "apache2::mod_proxy_balancer"
+include_recipe "apache2::mod_proxy_http"
+include_recipe "apache2::mod_rewrite"
+include_recipe "awscli"
 
 cookbook_file "/etc/apache2/sites-available/cabinjohnorganizing.com.conf" do
         source "/sites-available/cabinjohnorganizing.com.conf"
@@ -18,16 +25,24 @@ end
 remote_directory "/opt/content/cabinjohnorganizing" do
         source "html"
 end
+=begin
+package "libapache2-mod-php5" do
+    action :install
+end
 
+%w[ proxy_http proxy_balancer proxy rewrite php5 ].each do |module|
+apache_module module do
+  enable true
+end
+=end
 bash "copy dorsetwest image" do
-	user "root"
-	code <--EOH
-		aws s3 cp s3://cjo-content/dorsetwest.tgz /opt/dorsetwest.tgz
-		cd /opt/content
-		tar xzf ../dorsetwest.tgz
-		ln -s drupal_20141130a drupal
-	EOH
-	not_if do ::File.exists?("/opt/content/dorsetwest") end
+        user "root"
+        code <<-EOH
+                aws s3 cp s3://cjo-content/dorsetwest.tgz /opt/dorsetwest.tgz
+                sudo tar xzf /opt/dorsetwest.tgz --directory=/opt/content/
+        EOH
+        not_if do ::File.exists?("/opt/content/dorsetwest") end
+end
 
 apache_site "cabinjohnorganizing.com" do
   enable true
